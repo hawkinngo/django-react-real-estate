1b2dba49e60b7fcd6584eaf5932dfc50c6d3ee16
Hawkin Ngo <hawkin.ngo@gmail.com>
2022-07-20 14:46:37 +0700

    react client + nginx + redux toolkit

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,10 @@
 import React from "react";
-import { Layout } from "antd";
-import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import AppFooter from "./components/common/AppFooter";
-import AppHeader from "./components/common/AppHeader";
-import HomePage from "./pages/HomePage";
-import PropertyListPage from "./pages/PropertyListPage";
-
-const { Content, Header, Footer } = Layout;
 
 const App = () => {
   return (
-    <Router>
-      <Layout className="main-layout">
-        <Header>
-          <AppHeader />
-        </Header>
-        <Content>
-          <Routes>
-            <Route exact path="/" element={<HomePage />}></Route>
-            <Route
-              exact
-              path="/properties"
-              element={<PropertyListPage />}
-            ></Route>
-          </Routes>
-        </Content>
-        <Footer>
-          <AppFooter />
-        </Footer>
-      </Layout>
-    </Router>
+    <div className="App">
+      <h1>Welcome to the Real Estate</h1>
+    </div>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import store from "./store";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = createRoot(document.getElementById("root"));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
